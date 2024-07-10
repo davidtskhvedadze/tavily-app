4008cdbbd820e1981ce102945136c8af295b945b
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-07-10 02:40:28 -0400

    Setup fullstack structure

---
+++ server/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+// const EXAMPLEROUTER = require('./controllers/EXAMPLE ROUTER')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose');
+
+
+mongoose.set('strictQuery',false)
+
+const url = config.MONGO_URI
+
+mongoose.connect(url)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+// app.use(express.static('dist'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+// app.use('/', EXAMPLE ROUTER)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+app.listen(config.PORT, () => {
+  logger.info(`Server is running on port ${config.PORT}`);
+});
---
+++ server/utils/middleware.js
@@ -0,0 +1,30 @@
+const morgan = require('morgan')
+const logger = require('./logger')
+
+morgan.token('body', (req) => JSON.stringify(req.body))
+
+const requestLogger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  } else if (error.name === 'MongoServerError' && error.code === 11000) {
+    return response.status(400).json({ error: 'duplicate key error' })
+  }
+
+  next(error)
+}
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler
+}
