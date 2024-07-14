497afa4dd1b760d4ff8b45ff34350bddf72737dc
davidtskhvedadze <127141011+davidtskhvedadze@users.noreply.github.com>
2024-07-13 21:50:27 -0400

    UI Build

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const dotenv = require('dotenv');
 const usersRouter = require('./controllers/usersRouter')
+const authRouter = require('./controllers/authRouter');
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose');
+const SpotifyWebAPI = require('spotify-web-api-node');
 
 
 mongoose.set('strictQuery',false)
@@ -21,10 +22,51 @@ mongoose.connect(url)
     logger.error('error connecting to MongoDB:', error.message)
   })
 
+const spotifyApi = new SpotifyWebAPI({
+  clientId: config.SPOTIFY_CLIENT_ID,
+  clientSecret: config.SPOTIFY_CLIENT_SECRET,
+  redirectUri: config.SPOTIFY_CALLBACK_URL
+});
+
 app.use(cors())
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
+
+app.get('/login', (req, res) => { 
+  const scopes = ['user-read-private', 'user-read-email'];
+  const authorizeURL = spotifyApi.createAuthorizeURL(scopes);
+  res.redirect(authorizeURL);
+})
+
+app.get('/callback', (req, res) => {
+  const error = req.query.error;
+  const code  = req.query.code;
+  const state = req.query.state;
+
+  if (error) {
+    res.send(`Callback Error: ${error}`);
+    return;
+  }
+   spotifyApi.authorizationCodeGrant(code)
+     .then(data => {
+      const access_token = data.body['access_token'];
+      const refresh_token = data.body['refresh_token'];
+      const expires_in = data.body['expires_in'];
+      spotifyApi.setAccessToken(access_token);
+      spotifyApi.setRefreshToken(refresh_token);
+      res.send('Success! You can now close the window.');
+
+      setInterval(async () => {
+        const data = await spotifyApi.refreshAccessToken();
+        const access_token = data.body['access_token'];
+        spotifyApi.setAccessToken(access_token);
+      }, expires_in / 2 * 1000);
+     })
+     .catch(error => {
+      res.send(`Callback Error: ${error}`);
+     });
+})
 
 app.use('/', usersRouter)
 
--- server/utils/config.js
+++ server/utils/config.js
@@ -2,8 +2,20 @@ require('dotenv').config()
 
 const PORT = process.env.PORT
 const MONGO_URI = process.env.MONGO_URI
+const SPOTIFY_CLIENT_ID = process.env.SPOTIFY_CLIENT_ID
+const SPOTIFY_CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET
+const SPOTIFY_CALLBACK_URL = process.env.SPOTIFY_CALLBACK_URL
+const AUTH_URL = process.env.AUTH_URL
+const TOKEN_URL = process.env.TOKEN_URL
+const API_BASE_URL = process.env.API_BASE_URL
 
 module.exports = {
   MONGO_URI,
-  PORT
+  PORT,
+  SPOTIFY_CLIENT_ID,
+  SPOTIFY_CLIENT_SECRET,
+  SPOTIFY_CALLBACK_URL,
+  AUTH_URL,
+  TOKEN_URL,
+  API_BASE_URL
 }
